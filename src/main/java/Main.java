6e5a0d37a85fed433bb17e0cf97cbf09bc716f94
Fabian Mathis <fabian.mathis@tum.de>
2025-01-21 17:20:00 +0100

    adjust parser and git ignore

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,10 +8,16 @@ import org.slf4j.LoggerFactory;
 public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
-        ArgumentParser parser = ArgumentParsers.newFor("Main").build().defaultHelp(true).description("Program that calculates the PSI values for the exons of a BAM file.");
-        parser.addArgument("-gtf").required(true).help("GTF File Path").metavar("<GTF file>").type(String.class);
-        parser.addArgument("-bam").required(true).help("BAM File Path").metavar("<BAM file>").type(String.class);
-        parser.addArgument("-o").required(true).help("Output Path").metavar("<output file path>").type(String.class);
+        ArgumentParser parser = ArgumentParsers.newFor("Main").build().defaultHelp(true).description("Go Enrich");
+        parser.addArgument("-obo").required(true).help("OBO File Path").metavar("<OBO file>").type(String.class);
+        parser.addArgument("-root").required(true).help("GO namespace").metavar("<GO namespace>").type(String.class);
+        parser.addArgument("-mapping").required(true).help("gene2go mapping file").metavar("<gene2go_mapping_file>").type(String.class);
+        parser.addArgument("-mappingtype").required(true).help("Mapping Type").metavar("[ensembl|go]").type(String.class);
+        parser.addArgument("-enrich").required(true).help("diffexp file").metavar("<diffexp_file>").type(String.class);
+        parser.addArgument( "-o").required(true).help("Output Path").metavar("<output file path>").type(String.class);
+        parser.addArgument( "-minsize").required(true).help("minsize").metavar("<min size>").type(Integer.class);
+        parser.addArgument( "-maxsize").required(true).help("maxsize").metavar("<max size>").type(Integer.class);
+        parser.addArgument( "-overlapout").required(false).help("maxsize").metavar("overlap_out_tsv").type(String.class);
         try {
             Namespace res = parser.parseArgs(args);
             var start = System.currentTimeMillis();
